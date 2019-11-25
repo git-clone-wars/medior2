fd58426c6fe7ebfb84cbf42ab34222c20939cb49
Enya <enya.baez@gmail.com>
2019-11-25 16:47:34 -0500

    inertial kermit

--- App.js
+++ App.js
@@ -1,19 +1,18 @@
-import { AppLoading } from 'expo';
-import { Asset } from 'expo-asset';
-import * as Font from 'expo-font';
-import React, { useState } from 'react';
-import { Platform, StatusBar, StyleSheet, View } from 'react-native';
-import { Ionicons } from '@expo/vector-icons';
-import {FirebaseWrapper} from './firebase/firebase'
-import {firebaseConfig} from './firebase/config'
+import { AppLoading } from 'expo'
+import { Asset } from 'expo-asset'
+import * as Font from 'expo-font'
+import React, { useState } from 'react'
+import { Platform, StatusBar, StyleSheet, View } from 'react-native'
+import { Ionicons } from '@expo/vector-icons'
+import { FirebaseWrapper } from './firebase/firebase'
+import { firebaseConfig } from './firebase/config'
 
-import AppNavigator from './navigation/AppNavigator';
+import AppNavigator from './navigation/AppNavigator'
 
 export default function App(props) {
+  FirebaseWrapper.getInstance().initialize(firebaseConfig)
 
-  FirebaseWrapper.GetInstance().Initialize(firebaseConfig)
-
-  const [isLoadingComplete, setLoadingComplete] = useState(false);
+  const [isLoadingComplete, setLoadingComplete] = useState(false)
 
   if (!isLoadingComplete && !props.skipLoadingScreen) {
     return (
@@ -22,14 +21,14 @@ export default function App(props) {
         onError={handleLoadingError}
         onFinish={() => handleFinishLoading(setLoadingComplete)}
       />
-    );
+    )
   } else {
     return (
       <View style={styles.container}>
-        {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
+        {Platform.OS === 'ios' && <StatusBar barStyle='default' />}
         <AppNavigator />
       </View>
-    );
+    )
   }
 }
 
@@ -46,17 +45,17 @@ async function loadResourcesAsync() {
       // remove this if you are not using it in your app
       'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
     }),
-  ]);
+  ])
 }
 
 function handleLoadingError(error) {
   // In this case, you might want to report the error to your error reporting
   // service, for example Sentry
-  console.warn(error);
+  console.warn(error)
 }
 
 function handleFinishLoading(setLoadingComplete) {
-  setLoadingComplete(true);
+  setLoadingComplete(true)
 }
 
 const styles = StyleSheet.create({
@@ -64,4 +63,4 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
   },
-});
+})
