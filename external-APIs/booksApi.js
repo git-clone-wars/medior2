b5fa1f702e64fdacc4004badb769658561305ee0
Enya <enya.baez@gmail.com>
2019-11-25 13:10:54 -0500

    note on booksAPIkey

--- external-APIs/booksApi.js
+++ external-APIs/booksApi.js
@@ -4,7 +4,7 @@ import axios from 'axios'
 const isbnScanSearch = async isbnNum => {
   try {
     const { data } = await axios.get(
-      `https://www.googleapis.com/books/v1/volumes?q=isbn:${isbnNum}&key=${booksAPIKey}`
+      `https://www.googleapis.com/books/v1/volumes?q=isbn:${isbnNum}&key=${booksAPIKey}` //might not work unless API key is directly put in
     )
     return data
   } catch (error) {
