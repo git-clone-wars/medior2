214c67c1fab2ce566adbf2c621203a85403c6b4a
Enya <enya.baez@gmail.com>
2019-11-25 12:11:10 -0500

    put isbn search func in scanner

--- screens/ScannerScreen.js
+++ screens/ScannerScreen.js
@@ -1,31 +1,32 @@
-import * as React from 'react';
-import { Text, View, StyleSheet, Button } from 'react-native';
-import * as Permissions from 'expo-permissions';
-import { BarCodeScanner } from 'expo-barcode-scanner';
+import * as React from 'react'
+import { Text, View, StyleSheet, Button } from 'react-native'
+import * as Permissions from 'expo-permissions'
+import { BarCodeScanner } from 'expo-barcode-scanner'
+import isbnScanSearch from '../external-APIs'
 
 export default class Scanner extends React.Component {
   state = {
     hasCameraPermission: null,
     scanned: false,
-  };
+  }
 
   async componentDidMount() {
-    this.getPermissionsAsync();
+    this.getPermissionsAsync()
   }
 
   getPermissionsAsync = async () => {
-    const { status } = await Permissions.askAsync(Permissions.CAMERA);
-    this.setState({ hasCameraPermission: status === 'granted' });
-  };
+    const { status } = await Permissions.askAsync(Permissions.CAMERA)
+    this.setState({ hasCameraPermission: status === 'granted' })
+  }
 
   render() {
-    const { hasCameraPermission, scanned } = this.state;
+    const { hasCameraPermission, scanned } = this.state
 
     if (hasCameraPermission === null) {
-      return <Text>Requesting for camera permission</Text>;
+      return <Text>Requesting for camera permission</Text>
     }
     if (hasCameraPermission === false) {
-      return <Text>No access to camera</Text>;
+      return <Text>No access to camera</Text>
     }
     return (
       <View
@@ -33,21 +34,26 @@ export default class Scanner extends React.Component {
           flex: 1,
           flexDirection: 'column',
           justifyContent: 'flex-end',
-        }}>
+        }}
+      >
         <BarCodeScanner
           onBarCodeScanned={scanned ? undefined : this.handleBarCodeScanned}
           style={StyleSheet.absoluteFillObject}
         />
 
         {scanned && (
-          <Button title={'Tap to Scan Again'} onPress={() => this.setState({ scanned: false })} />
+          <Button
+            title={'Tap to Scan Again'}
+            onPress={() => this.setState({ scanned: false })}
+          />
         )}
       </View>
-    );
+    )
   }
 
   handleBarCodeScanned = ({ type, data }) => {
-    this.setState({ scanned: true });
-    alert(`Bar code with type ${type} and data ${data} has been scanned!`);
-  };
+    this.setState({ scanned: true })
+    isbnScanSearch(data)
+    alert(`Bar code with type ${type} and data ${data} has been scanned!`)
+  }
 }
