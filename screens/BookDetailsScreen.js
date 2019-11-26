5641665977772db33d2fd911e742a927c72198e0
Enya <enya.baez@gmail.com>
2019-11-26 17:47:49 -0500

    not all book info has authors array

--- screens/BookDetailsScreen.js
+++ screens/BookDetailsScreen.js
@@ -6,9 +6,10 @@ export default class BookDetails extends React.Component {
   render() {
     const { navigation } = this.props
     const book = navigation.getParam('book')
+    console.log(book)
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text style={{ fontSize: 30 }}>Is this the book? {book.title} by {book.authors[0]} </Text>
+        <Text style={{ fontSize: 30 }}>Is this the book? {book.title} </Text>
         <Button
           onPress={() => this.props.navigation.goBack()}
           title='Dismiss'
