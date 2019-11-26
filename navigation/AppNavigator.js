80c415146db4b0e27143c2807c42bd97c326adbf
Greta Gadbois <ggadbois@hmc.edu>
2019-11-26 16:27:27 -0500

    init commit

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import SignUp-Login from './'
 
 import MainTabNavigator from './MainTabNavigator';
 
@@ -7,6 +8,8 @@ export default createAppContainer(
   createSwitchNavigator({
     // You could add another route here for authentication.
     // Read more at https://reactnavigation.org/docs/en/auth-flow.html
+    AuthLoading: AuthLoadingScreen,
     Main: MainTabNavigator,
+    Auth: SignUpLogin
   })
 );
