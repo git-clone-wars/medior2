812b7be21ede0c4e32e8a2d2987eb1c8d672966e
Greta Gadbois <ggadbois@hmc.edu>
2019-11-27 00:05:07 -0500

    can log in/sign up at beginning of app and continue to rest of app

--- screens/AuthLoadingScreen.js
+++ screens/AuthLoadingScreen.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react'
 import {
   ActivityIndicator,
   AsyncStorage,
@@ -6,26 +6,23 @@ import {
   StyleSheet,
   Text,
   View,
-} from 'react-native';
+} from 'react-native'
 import FirebaseWrapper from '../firebase/firebase'
-
 
 export default class AuthLoadingScreen extends Component {
   async componentDidMount() {
-    const auth = await FirebaseWrapper.getInstance().auth()
-    console.log(auth)
-    this.props.navigation.navigate(auth.currentUser ? 'Main' : 'Auth');
-
+    const auth = await FirebaseWrapper.getInstance().wrapperAuth()
+    this.props.navigation.navigate(auth.currentUser ? 'Main' : 'Auth')
   }
 
   // Fetch the token from storage then navigate to our appropriate place
   _bootstrapAsync = async () => {
-    const userToken = await AsyncStorage.getItem('userToken');
+    const userToken = await AsyncStorage.getItem('userToken')
 
     // This will switch to the App screen or Auth screen and this loading
     // screen will be unmounted and thrown away.
-    this.props.navigation.navigate(userToken ? 'Main' : 'Auth');
-  };
+    this.props.navigation.navigate(userToken ? 'Main' : 'Auth')
+  }
 
   // Render any loading content that you like here
   render() {
@@ -33,8 +30,8 @@ export default class AuthLoadingScreen extends Component {
       <View>
         {/* <ActivityIndicator /> */}
         <Text>Loading</Text>
-        <StatusBar barStyle="default" />
+        <StatusBar barStyle='default' />
       </View>
-    );
+    )
   }
 }
