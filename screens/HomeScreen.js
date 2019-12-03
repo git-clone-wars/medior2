f6e85a35485ddb6c65f3ac9c00e3035d75b883dc
Enya <enya.baez@gmail.com>
2019-12-03 12:54:34 -0500

    no more material UI >:(

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -6,63 +6,19 @@ import {
   StyleSheet,
   Text,
   View,
+  SafeAreaView,
 } from 'react-native'
 
-import {
-  makeStyles,
-  ExpansionPanel,
-  ExpansionPanelDetails,
-  ExpansionPanelSummary,
-} from '@material-ui/core'
 
-import ExpandMoreIcon from '@material-ui/icons'
+export default class HomeScreen extends React.Component {
 
-export default function HomeScreen() {
-  const classes = useStyles()
-  const [expanded, setExpanded] = React.useState(false)
-
-  const handleChange = panel => (event, isExpanded) => {
-    setExpanded(isExpanded ? panel : false)
+  render() {
+    return (
+      <SafeAreaView>
+        <Text>Helloooo</Text>
+      </SafeAreaView>
+    )
   }
-
-  return (
-    <View className={classes.root}>
-      <ExpansionPanel
-        expanded={expanded === 'panel1'}
-        onChange={handleChange('panel1')}
-      >
-        <ExpansionPanelSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls='panel1bh-content'
-          id='panel1bh-header'
-        >
-          <Text className={classes.heading}>Currently Watching</Text>
-          <Text className={classes.secondaryHeading}>
-            I am an expansion panel
-          </Text>
-        </ExpansionPanelSummary>
-        <ExpansionPanelDetails>
-          <Text>
-            Nulla facilisi. Phasellus sollicitudin nulla et quam mattis feugiat.
-            Aliquam eget maximus est, id dignissim quam.
-          </Text>
-        </ExpansionPanelDetails>
-      </ExpansionPanel>
-    </View>
-  )
 }
 
-const useStyles = makeStyles(theme => ({
-  root: {
-    width: '100%',
-  },
-  heading: {
-    fontSize: theme.typography.pxToRem(15),
-    flexBasis: '33.33%',
-    flexShrink: 0,
-  },
-  secondaryHeading: {
-    fontSize: theme.typography.pxToRem(15),
-    color: theme.palette.text.secondary,
-  },
-}))
+
